b528b710d0132b1433db34c3bfe18810473eaaa1
thevolcanomanishere <alex.mcgonagle.dev@gmail.com>
2024-07-19 13:22:02 +0100

    Remove log

--- src/index.ts
+++ src/index.ts
@@ -70,7 +70,6 @@ fastify.get('/client-count', async (request, reply) => {
 const start = async () => {
   try {
     await fastify.listen({ port: 3000, host: '0.0.0.0' });
-    console.log('Server is running on http://localhost:3000');
     // Generate initial price
     await fetchLatestPrice();
   } catch (err) {
